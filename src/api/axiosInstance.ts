e0ab09db5817806eb0878cc3636130ad8052cfbb
Danushadha Amarasekera <danushadha@live.com>
2026-09-14 13:18:17 +0530

    Update API URL for Railway backend

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8082"
+    baseURL: "https://daring-imagination-production-57b7.up.railway.app"
 });
 
 api.interceptors.request.use((config) => {
