c5e74b047ff1d48b8eb9cecf1f9595b45dff489a
Danushadha Amarasekera <danushadha@live.com>
2026-09-20 23:41:04 +0530

    item Update and disable done

--- src/api/itemApi.ts
+++ src/api/itemApi.ts
@@ -1,11 +1,21 @@
-import api from "./axiosInstance"
-import type { ItemData} from "../utils/type";
+    import api from "./axiosInstance"
+    import type { ItemData} from "../utils/type";
 
-export const saveItem= async(item:ItemData)=>{
-    return await api.post("/api/itemDetailsController", item)
-   
-}
+    export const saveItem= async(item:ItemData)=>{
+        return await api.post("/api/itemDetailsController", item)
+    
+    }
+    export const updateItem= async(item:ItemData)=>{
+        return await api.post("/api/itemDetailsController/update", item)
+    
+    }
+    export const disableItem= async(item:ItemData)=>{
+        return await api.post("/api/itemDetailsController/disable", item)
+    
+    }
 
-export const getAllItems = async () => {
-    return await api.get("/api/itemDetailsController");
-};
+
+    export const getAllItems = async () => {
+        return await api.get("/api/itemDetailsController");
+    };
+
--- src/components/CreateIteComp.tsx
+++ src/components/CreateIteComp.tsx
@@ -3,7 +3,11 @@ import styles from "../components/styles/CreateIteComp.module.css"
 import type { ItemData } from "../utils/type"
 import { useState } from "react"
 import { saveItem } from "../api/itemApi"
+import { getAllItems } from "../api/itemApi"
+import { updateItem } from "../api/itemApi"
+import { disableItem } from "../api/itemApi"
 import ItemImg from "../assets/images/itemcrtImg.jpeg"
+
 
 function CreateIteComp() {
     const [itemdata, setItemdata] = useState<ItemData>({
@@ -13,6 +17,9 @@ function CreateIteComp() {
         qty: 0
 
     })
+
+    const [changeName, setChangeName] = useState("Save Item")
+    const [loadItemsData, setloadItemsData] = useState<ItemData[]>([])
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 
@@ -24,15 +31,40 @@ function CreateIteComp() {
 
     }
 
-    const saveItemdtls = async () => {
+    const crudhandle = async () => {
 
+        //this prevents user type without selecting a item
+        if (itemdata.id < 1) {
+            alert("You've not select a Item")
+            return
+        }
         try {
 
-
-            const response = await saveItem(itemdata)
-            console.log(response.data)
+            if (changeName === "Save Item") {
 
 
+
+                const response = await saveItem(itemdata)
+
+                console.log(response.data)
+
+            }
+            if (changeName === "Update Item") {
+
+
+                const response = await updateItem(itemdata)
+
+                console.log(response.data)
+
+            }
+
+            if (changeName === "Disable Item") {
+
+                const response = await disableItem(itemdata)
+
+                console.log(response.data)
+
+            }
 
             setItemdata({
                 id: 0,
@@ -48,6 +80,50 @@ function CreateIteComp() {
 
     }
 
+
+
+    const loadData = async () => {
+
+        try {
+            const response = await getAllItems()
+            console.log("loading Items", response.data)
+
+            setloadItemsData(response.data)
+
+        }
+        catch (error) {
+            console.log(error)
+
+        }
+
+
+    }
+
+
+    const handleEdit = async () => {
+
+        await loadData()
+        setChangeName("Update Item")
+
+    }
+
+    const handleDelete = async () => {
+        await loadData()
+        setChangeName("Disable Item")
+
+    }
+
+    const closeItempnl = () => {
+        setChangeName("Save Item")
+        setItemdata({
+            id: 0,
+            itemName: "",
+            price: 0,
+            qty: 0
+        })
+
+    }
+
     return (
         <>
             <div className={styles.mainContent}>
@@ -55,14 +131,64 @@ function CreateIteComp() {
 
                 <div className={styles.formContent}>
                     <div className={styles.imgside}>
+                        {(changeName === "Update Item" || changeName === "Disable Item") && (
+
+                            <div className={styles.viewItemsPnl}>
+                                <div className={styles.itmpnlheader}>
+                                    <h3>Select Item</h3>
+                                    <button onClick={closeItempnl}>X</button>
+                                </div>
+
+                                <table>
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Item Name</th>
+                                            <th>Item Price</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {
+                                            loadItemsData.map((item, index) => (
+                                                <tr key={item.id}>
+
+                                                    <td>{index + 1}</td>
+                                                    <td><button className={styles.itmBtn} onClick={() => setItemdata(item)}>{item.itemName}</button></td>
+                                                    <td><button className={styles.itmBtn} onClick={() => setItemdata(item)}>Rs. {Number(item.price).toFixed(2)}</button></td>
+
+                                                </tr>
+
+                                            ))
+                                        }
+
+                                    </tbody>
+                                </table>
+
+
+                            </div>
+
+                        )}
+
+
                         <img src={ItemImg} alt="" />
+
                     </div>
 
                     <div className={styles.formSide}>
-                        <h1>Create Item</h1>
+                        <h1>{changeName}</h1>
+                        <div className={styles.crudBtn}>
+
+                            <button onClick={handleEdit}>Edit</button>
+                            <button onClick={handleDelete}>Disable </button>
+
+                        </div>
                         <div className={styles.formRow}>
                             <label>Item Name :-</label>
-                            <input type="text" name="itemName" value={itemdata.itemName} onChange={handleChange} autoComplete="off" />
+                            <input type="text" name="itemName" value={itemdata.itemName} 
+                            onChange={handleChange} 
+                            autoComplete="off"
+                            disabled={changeName === "Disable Item"}
+                            />
 
                         </div>
 
@@ -70,11 +196,15 @@ function CreateIteComp() {
                         <div className={styles.formRow}>
                             <label>Price :- </label>
 
-                            <input type="text" name="price" value={itemdata.price} onChange={handleChange} autoComplete="off" />
+                            <input type="text" name="price"
+                             value={itemdata.price} 
+                             onChange={handleChange}
+                              autoComplete="off"
+                              disabled={changeName === "Disable Item"} />
 
                         </div>
                         <div className={styles.btnPannel}>
-                            <button onClick={saveItemdtls}>Save Item</button>
+                            <button onClick={crudhandle}>{changeName}</button>
 
                         </div>
 
